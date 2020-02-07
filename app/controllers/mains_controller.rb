e90f843c381f0a43b919157b760556dbd09249d3
Winy81 <Adam.Nemeth@FootballPools.com>
2020-02-07 10:30:07 +0000

    setup basic code structure

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,2 +1,9 @@
 class MainsController < ApplicationController
+  def index
+    @message = "Hello from Main"
+  end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :mains, only: [:index, :show]
+
+  root to: 'mains#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
