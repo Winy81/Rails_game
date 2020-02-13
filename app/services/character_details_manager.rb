ac96a58f1ef5be21305e41bd4519b20ced84cf50
Winy81 <Adam.Nemeth@FootballPools.com>
2020-02-13 10:23:22 +0000

    add character basic details reductor

---
+++ app/services/character_details_manager.rb
@@ -0,0 +1,10 @@
+class CharacterDetailsManager
+
+  def hold
+    data = Character.all
+    data.each do |details|
+      details.update_attributes(fed_state: details.fed_state -=1)
+    end
+  end
+
+end
