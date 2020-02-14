4ee900847b104e70bb074c981d9c154480023995
Winy81 <Adam.Nemeth@FootballPools.com>
2020-02-14 09:19:50 +0000

    Devise has been instaled, the User table has been created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :mains, only: [:index, :show]
 
   root to: 'mains#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200210081823) do
+ActiveRecord::Schema.define(version: 20200214091613) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name",                                     null: false
@@ -23,5 +23,19 @@ ActiveRecord::Schema.define(version: 20200210081823) do
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
