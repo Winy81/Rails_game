d70bad3655d12bc12dd1cb04f64eb73c6d9780c9
Winy81 <Adam.Nemeth@FootballPools.com>
2020-03-23 17:14:58 +0000

    working on mains#index using username vit helper method, basic desing setup

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,8 @@ class Character < ActiveRecord::Base
 
   belongs_to :user
 
-  def self.age_order_alive_filter
-    order(:age => :desc).where(status:"alive")
+  def self.age_order_filter
+    order(:age => :desc)
   end
 
 end
