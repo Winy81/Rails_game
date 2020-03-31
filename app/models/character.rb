8301ac1d48542df74cf5bc777c81bb2f8dce5aba
Winy81 <Adam.Nemeth@FootballPools.com>
2020-03-31 15:10:29 +0100

    small style changes and add validations for character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,11 @@ class Character < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :name, length: { maximum: 12 }
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :user_id, presence: true
+
   def self.age_order_filter
     order(:age => :desc)
   end
