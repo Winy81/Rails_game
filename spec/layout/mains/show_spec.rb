b8e4e291b227f3daf6ed5bba0e83f9df9e209ac9
Winy81 <Adam.Nemeth@FootballPools.com>
2020-04-03 23:43:29 +0100

    repair on the test environment setup

--- spec/layout/mains/show_spec.rb
+++ spec/layout/mains/show_spec.rb
@@ -5,25 +5,25 @@ include DateTransformHelper
 RSpec.feature 'Show characters without user logged in' do
 
   before do
-    @user = User.create(name:'Test_user', email:'email@email.com', password:'password')
-    @character = Character.create(name:'Test_character', user_id:@user.id)
-    @number_of_characters = Character.where(user_id:@user.id)
+    @user_main_show = User.create!(name:'user_main_show', email:'user_main_show@email.com', password:'password', password_confirmation:'password')
+    @character_main_show = Character.create!(name:'character_main_show', user_id:@user_main_show.id)
+    @number_of_characters = Character.where(user_id:@user_main_show.id)
   end
 
   scenario 'shows character details' do
     visit '/'
-    click_link 'Test_character'
+    click_link 'character_main_show'
 
     expect(page).to have_content('sign up')
     expect(page).to have_content('log-in')
 
     expect(page).to have_content('Details of Character:')
-    expect(page).to have_content(@character.name)
-    expect(page).to have_content(@character.age)
-    expect(page).to have_content(@character.status)
+    expect(page).to have_content(@character_main_show.name)
+    expect(page).to have_content(@character_main_show.age)
+    expect(page).to have_content(@character_main_show.status)
 
-    expect(page).to have_content(@user.name)
-    expect(page).to have_content(@user.date_view_optimizer(@user.created_at))
+    expect(page).to have_content(@user_main_show.name)
+    expect(page).to have_content(@user_main_show.date_view_optimizer(@user_main_show.created_at))
 
     expect(@number_of_characters.count).to eq(1)
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-include Warden::Test::Helpers
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -36,6 +36,24 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include Warden::Test::Helpers
+
+  config.after :each do
+    Warden.test_reset!
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
