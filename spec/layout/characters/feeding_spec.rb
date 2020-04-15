24cf85bceb81c882aaa41aa479b46aed287dfbe8
Winy81 <Adam.Nemeth@FootballPools.com>
2020-04-15 11:16:47 +0100

    test of feeding_spec.rb

--- spec/layout/characters/feeding_spec.rb
+++ spec/layout/characters/feeding_spec.rb
@@ -10,14 +10,29 @@ RSpec.feature 'Feeding page' do
                                     user_id:1,
                                     status:'alive',
                                     age: 214,
-                                    fed_state:56,
+                                    fed_state:10,
                                     activity_require_level:46,
                                     happiness:31)
   end
 
   scenario 'Should turn up with list of food' do
 
+    character_id = @char_of_feeding_page.id
+    character_current_fed_state = @char_of_feeding_page.fed_state
+
     visit "/character/#{@char_of_feeding_page.id}/feeding"
 
+    expect(page).to have_content('Your Fed State is:')
+    expect(page).to have_content(character_current_fed_state)
+
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process?extra=from_feeding&fed_state=#{character_current_fed_state + 5}')]")
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process?extra=from_feeding&fed_state=#{character_current_fed_state + 10}')]")
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process?extra=from_feeding&fed_state=#{character_current_fed_state + 15}')]")
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process?extra=from_feeding&fed_state=#{character_current_fed_state + 20}')]")
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process?extra=from_feeding&fed_state=#{character_current_fed_state + 25}')]")
+
+    page.should have_xpath("//a[contains(@href,'character/#{character_id}/feeding_process')]", :count => 5)
+
+    page.should have_xpath("//a[contains(@href,'/characters/#{character_id}'")
   end
 end
