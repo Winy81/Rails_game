6e76f83935738fc9f6da0ae48e1e5d018882d816
Winy81 <Adam.Nemeth@FootballPools.com>
2020-05-16 21:54:50 +0100

    setup full test of character#new

--- spec/layout/characters/new_spec.rb
+++ spec/layout/characters/new_spec.rb
@@ -11,26 +11,47 @@ RSpec.feature 'Feeding process page' do
 
     scenario 'Should be not proceed and create a character' do
 
+      number_of_character_before = Character.where(user_id:1).count
+      expect(number_of_character_before).to eq(0)
+
+      visit 'characters'
+      find(:xpath, "//a[contains(@href,'/characters/new')]").click
+
+      fill_in 'character_name', with: 'char_of_new'
+      click_button 'Create Character'
+
+      number_of_character_after = Character.where(user_id:1).count
+      current_character = Character.find_by(user_id:1)
+
+      current_path.should == character_path(current_character)
+      character_activity_path(current_character)
+      expect(number_of_character_after).to eq(1)
 
     end
-
   end
 
   feature 'When the user has character alive' do
 
-    @char_of_new = Character.create(name:'@char_of_new',
-                                    user_id:1,
-                                    status:'alive',
-                                    age: 214,
-                                    fed_state:50,
-                                    activity_require_level:5,
-                                    happiness:50)
+    before do
+      @char_of_new = Character.create(name:'@char_of_new',
+                                      user_id:1,
+                                      status:'alive',
+                                      age: 214,
+                                      fed_state:50,
+                                      activity_require_level:5,
+                                      happiness:50)
+    end
 
     scenario 'Should be not proceed and redirected for character page with message' do
 
+      number_of_character = Character.where(user_id:1).count
+
+      visit 'characters/new'
+
+      current_path.should == characters_path
+      expect(page).to have_content('You have a character Alive')
+      expect(number_of_character).to eq(1)
 
     end
-
   end
-
 end
