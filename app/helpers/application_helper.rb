35a23d79e0ddadac9abad55a1e564e37d00083db
Winy81 <Adam.Nemeth@FootballPools.com>
2020-06-01 20:40:24 +0100

    manage the wallet shows in the screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,18 @@ module ApplicationHelper
     end
   end
 
+  def wallet_view
+    if current_user && has_user_created_character != 0
+      current_users_wallet = Wallet.find_by(user_id:current_user.id)
+      output = "Wallet:  #{current_users_wallet.amount} Gold"
+      output
+    end
+  end
+
+  private
+
+  def has_user_created_character
+    Character.where(user_id:current_user.id).count
+  end
+
 end
