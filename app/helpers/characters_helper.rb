419d69ad8084816a73f03c839e76cb442918d784
Winy81 <Adam.Nemeth@FootballPools.com>
2020-06-11 10:17:30 +0100

    setup a partial for pirce view in action

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -20,4 +20,12 @@ module CharactersHelper
     "#{full_days} Days and #{hours} Hours"
   end
 
+  def costs_amount_on_actions(amount)
+    "Going to Cost: #{amount} Gold"
+  end
+
+  def reached_amount_on_actions(amount)
+    "Going to Get: #{amount} Gold"
+  end
+  
 end
