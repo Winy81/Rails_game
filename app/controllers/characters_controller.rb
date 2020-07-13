cf0d50512546506e358aca944f83d5cba5574674
Winy81 <Adam.Nemeth@FootballPools.com>
2020-07-13 10:58:51 +0100

    layout upgrade on playing_process

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -108,13 +108,16 @@ class CharactersController < ApplicationController
   end
 
   def playing_process
-    #test required
     @current_amount = params[:amount].to_i
     @spendable_amount = -1*(@current_user.wallet.amount - @current_amount)
     @current_fed_state = params[:fed_state]
     @current_activity_state = params[:activity_require_level]
     @current_happiness_state = params[:happiness]
-    @sent_playing_points = -1*(@character.happiness.to_i - @current_happiness_state.to_i)
+
+    @sent_potion_of_food = -1*(@character.fed_state.to_i - @current_fed_state.to_i)
+    @sent_points_of_activity = -1*(@character.activity_require_level.to_i - @current_activity_state.to_i)
+    @sent_happiness_points = -1*(@character.happiness.to_i - @current_happiness_state.to_i)
+
     @costs = wallet_view - @current_amount
   end
 
