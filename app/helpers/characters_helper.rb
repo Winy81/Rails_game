94142163d635f19f0530add035bbe9f982c356cf
Winy81 <Adam.Nemeth@FootballPools.com>
2020-07-13 10:44:44 +0100

    add the new params buidler method for the palying process

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -42,6 +42,14 @@ module CharactersHelper
      extra: source }
   end
 
+  def params_builder_of_playing_process(fed_state,amount,activity_state,happiness_state,source)
+    {fed_state: fed_state,
+     amount: amount,
+     activity_require_level: activity_state,
+     happiness: happiness_state,
+     extra: source }
+  end
+
   private
 
   def result(seconds)
