5086e2547d2015b35cdaa3e8a458d09f0f53a2a9
Winy81 <Adam.Nemeth@FootballPools.com>
2020-07-28 07:24:37 +0100

    decrase number of method on process params builders. Using only two method insted of 6

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -7,7 +7,7 @@ module CharactersHelper
     result(length)
   end
 
-  def params_builder_of_feeding(character,wallet,fed_points,cost,happiness_points,activity_points,source)
+  def params_builder_of_action(character,wallet,fed_points,cost,happiness_points,activity_points,source)
     params_of_fed_state = character.fed_state + fed_points
     params_of_happiness = character.happiness + happiness_points
     params_of_activity = character.activity_require_level + activity_points
@@ -20,49 +20,7 @@ module CharactersHelper
      extra: source}
   end
 
-  def params_builder_of_activity(character,wallet,fed_points,cost,happiness_points,activity_points,source)
-    params_of_fed_state = character.fed_state + fed_points
-    params_of_happiness = character.happiness + happiness_points
-    params_of_activity = character.activity_require_level + activity_points
-    params_of_amount = wallet + cost
-
-    {fed_state: params_of_fed_state,
-     amount: params_of_amount,
-     happiness: params_of_happiness,
-     activity_require_level: params_of_activity,
-     extra: source}
-  end
-
-  def params_builder_of_playing(character,wallet,fed_points,cost,happiness_points,activity_points,source)
-    params_of_fed_state = character.fed_state + fed_points
-    params_of_happiness = character.happiness + happiness_points
-    params_of_activity = character.activity_require_level + activity_points
-    params_of_amount = wallet + cost
-
-    {fed_state: params_of_fed_state,
-     amount: params_of_amount,
-     happiness: params_of_happiness,
-     activity_require_level: params_of_activity,
-     extra: source}
-  end
-
-  def params_builder_of_feeding_process(fed_state,amount,activity_state,happiness_state,source)
-    {fed_state: fed_state,
-     amount: amount,
-     activity_require_level: activity_state,
-     happiness: happiness_state,
-     extra: source }
-  end
-
-  def params_builder_of_playing_process(fed_state,amount,activity_state,happiness_state,source)
-    {fed_state: fed_state,
-     amount: amount,
-     activity_require_level: activity_state,
-     happiness: happiness_state,
-     extra: source }
-  end
-
-  def params_builder_of_activity_process(fed_state,amount,activity_state,happiness_state,source)
+  def params_builder_of_action_process(fed_state,amount,activity_state,happiness_state,source)
     {fed_state: fed_state,
      amount: amount,
      activity_require_level: activity_state,
