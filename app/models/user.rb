5cc3126aa49fb47f5180c574ed2b01c4e9e7aa55
Winy81 <Adam.Nemeth@FootballPools.com>
2020-09-23 13:21:45 +0100

    implement role module into User model for white filter the possible values of role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  module Role
+    ADMIN = 'admin'.freeze
+    USER = 'user'.freeze
+    ALL = [ADMIN, USER]
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :name
+  validates_presence_of :role
+  validates_inclusion_of :role, in: Role::ALL
 
   has_many :characters, dependent: :destroy
   has_one :wallet, dependent: :destroy
