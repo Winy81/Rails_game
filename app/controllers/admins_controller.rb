25d10ecfc7d6bbd5e30c422441a81d84d0875d64
Winy81 <Adam.Nemeth@FootballPools.com>
2020-10-12 12:31:41 +0100

    add user_admin helper and action filter

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,15 @@
 class AdminsController < ApplicationController
 
-  def index
+  before_action :is_user_admin?
 
+  def index
+    binding.pry
+  end
+
+  private
+
+  def is_user_admin?
+    current_user.role == "admin"
   end
 
 end
