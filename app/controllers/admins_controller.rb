4780a7d485356f787dd31c695b498139135ab308
Winy81 <Adam.Nemeth@FootballPools.com>
2020-10-12 12:59:14 +0100

    correction on the user_admin before filter method. Move out the refactored character redirected methods form characters contrroler inot the application controller. Use this for redirection in the admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,10 +6,18 @@ class AdminsController < ApplicationController
 
   end
 
+  def account_management
+
+  end
+
+  def character_management
+
+  end
+
   private
 
   def is_user_admin?
-    current_user.role == "admin"
+    current_user.role == "admin" ? true : redirection_to_characters_path('alert', 'you have no admin privileges')
   end
 
 end
