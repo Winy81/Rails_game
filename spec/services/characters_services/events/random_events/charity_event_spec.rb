3638b81c6e758d742ab2096aed4985ca57931a02
Winy81 <Adam.Nemeth@FootballPools.com>
2020-12-20 06:44:06 +0000

    Test of charity_event_spec.rb

--- spec/services/characters_services/events/random_events/charity_event_spec.rb
+++ spec/services/characters_services/events/random_events/charity_event_spec.rb
@@ -6,15 +6,28 @@ describe CharactersServices::Events::RandomEvents::CharityEvent do
   let(:gold_for_lost) { CharactersServices::Events::RandomEvents::CharityEvent::GOLD_FOR_LOST }
   let(:description) { CharactersServices::Events::RandomEvents::CharityEvent::DESCRIPTION }
   let(:event_name) { described_class.to_s.split("::").last }
-  let(:character) { double(Character) }
   let(:event) { double(Event) }
 
   context 'When the event has been called' do
 
+    let(:character) { double('Character', id:1, user_id: 1) }
+    let(:characters) { [ character ] }
+    let(:user_of_character) { double('User', id:1) }
+    let(:budget) {double('Wallet', id:1, amount:20 )}
+    let(:updated_budget) { budget.amount - gold_for_lost }
+
     it 'should update the characters owner budget with GOLD_FOR_LOST' do
 
+      expect(Character).to receive(:active_living_characters).and_return(characters)
+      allow(character).to receive(:user).and_return(user_of_character)
+      expect(user_of_character).to receive(:budget).and_return(budget.amount)
+      expect(user_of_character).to receive(:set_budget).and_return(updated_budget)
+      expect(Event).to receive(:create).with(event_id:id,event_name:event_name,description:description)
+
+      CharactersServices::Events::RandomEvents::CharityEvent.new().process
+
     end
-    
+
   end
 
 end
