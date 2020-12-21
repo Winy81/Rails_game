6f17f0e4829ffee83b169f18ec864ac1c32ee55b
Winy81 <Adam.Nemeth@FootballPools.com>
2020-12-21 09:52:21 +0000

    Happy path of character_time_pass_manager_spec.rb

--- spec/services/characters_services/events/special_events/character_time_pass_manager_spec.rb
+++ spec/services/characters_services/events/special_events/character_time_pass_manager_spec.rb
@@ -10,11 +10,17 @@ describe CharactersServices::Events::SpecialEvents::CharacterTimePassManager do
 
     context 'When the character updated and still alive' do
 
-      let(:character) { double('Character', id:1, user_id: 1) }
+      let(:character) { double('Character', id:1, user_id: 1, fed_state:10) }
       let(:updated_character) { double('Character') }
       let(:characters) { [ character ] }
 
       it 'should update the character successfully ' do
+
+        expect(Character).to receive(:active_living_characters).and_return(characters)
+        allow(character).to receive(:simulated_time_passed_updated).and_return(updated_character)
+        expect(character).to receive(:reload).and_return(updated_character)
+        expect(character).to receive(:fed_state).and_return(9)
+        expect(Event).to receive(:create).with(event_id:id,event_name:event_name,description:description)
 
         CharactersServices::Events::SpecialEvents::CharacterTimePassManager.new().process
 
@@ -23,7 +29,7 @@ describe CharactersServices::Events::SpecialEvents::CharacterTimePassManager do
 
     context 'When the character updated and dying' do
 
-      let(:character) { double('Character', id:1, user_id: 1) }
+      let(:character) { double('Character', id:1, user_id: 1 , fed_state:1) }
       let(:updated_character) { double('Character') }
       let(:characters) { [ character ] }
       let(:user_of_character) { double('User', id:1) }
