811d55674b33b1ceac460c62dc08a90d326d6834
Winy81 <Adam.Nemeth@FootballPools.com>
2020-12-27 01:32:36 +0000

    Refactor on character_special_event_runner.rb

--- lib/jobs/character_special_event_runner.rb
+++ lib/jobs/character_special_event_runner.rb
@@ -1,34 +1,43 @@
 module Jobs
   class CharacterSpecialEventRunner
 
-    CHRISTMAS_DATE = "12-24--06-00"
-    NEW_YEARS_EVE_DATE = "01-01--00-00"
-    HALLOWEEN_EVENT = "10-31--20-00"
+    module SpecialEventList
+      EVENT_1 = CharactersServices::Events::SpecialEvents::CharacterTimePassManager
+      EVENT_2 = CharactersServices::Events::SpecialEvents::ChristmasEvent
+      EVENT_3 = CharactersServices::Events::SpecialEvents::NewYearsEvent
+      EVENT_4 = CharactersServices::Events::SpecialEvents::HalloweenEvent
+    end
+
+    module DatesOfSpecialEvents
+      CHRISTMAS_DATE = "12-24--06-00"
+      NEW_YEARS_EVE_DATE = "01-01--00-00"
+      HALLOWEEN_EVENT = "10-31--20-00"
+    end
 
     def perform
       current_date_time
-      christmas_event if current_date_time == CHRISTMAS_DATE
-      new_year_event if current_date_time == NEW_YEARS_EVE_DATE
-      halloween_event if current_date_time == HALLOWEEN_EVENT
+      christmas_event if current_date_time == DatesOfSpecialEvents::CHRISTMAS_DATE
+      new_year_event if current_date_time == DatesOfSpecialEvents::NEW_YEARS_EVE_DATE
+      halloween_event if current_date_time == DatesOfSpecialEvents::HALLOWEEN_EVENT
       time_pass_event
     end
 
     private
 
     def time_pass_event
-      CharactersServices::Events::SpecialEvents::CharacterTimePassManager.new().process
+      EVENT_1.new().process
     end
 
     def christmas_event
-      CharactersServices::Events::SpecialEvents::ChristmasEvent.new().process
+      EVENT_2.new().process
     end
 
     def new_year_event
-      CharactersServices::Events::SpecialEvents::NewYearsEvent.new().process
+      EVENT_3.new().process
     end
 
     def halloween_event
-      CharactersServices::Events::SpecialEvents::HalloweenEvent.new().process
+      EVENT_4.new().process
     end
 
     def current_date_time
