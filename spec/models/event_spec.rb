1d9e8d77c69cd2b6fa2a7d79e2808bbb763c50c8
Winy81 <Adam.Nemeth@FootballPools.com>
2020-12-28 09:01:54 +0000

    Upgrade on the test of Event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,9 +4,52 @@ RSpec.describe Event, type: :model do
 
   context 'validations' do
 
+    context 'built in validations' do
+
       it { is_expected.to validate_presence_of :event_id }
       it { is_expected.to validate_presence_of :event_name }
       it { is_expected.to validate_presence_of :description }
 
+    end
+
+    context 'custom validations' do
+
+      context 'exist Event' do
+
+        context 'valid' do
+
+          let(:event_id) { CharactersServices::Events::RandomEvents::CharityEvent::ID }
+          let(:event_name) { 'test' }
+          let(:description) { 'description of test event'}
+
+          it 'Should create an Event' do
+
+            current_number_of_event = Event.count
+            Event.create(event_id:event_id,event_name:event_name,description:description)
+            updated_number_of_event = Event.count
+            expect(updated_number_of_event).to eq(current_number_of_event + 1)
+
+          end
+        end
+
+        context 'invalid' do
+
+          let(:event_id) { 123456 }
+          let(:event_name) { 'test' }
+          let(:description) { 'description of test event'}
+
+          it 'Should NOT create an Event' do
+
+            current_number_of_event = Event.count
+            event = Event.create(event_id:event_id,event_name:event_name,description:description)
+            updated_number_of_event = Event.count
+            expect(updated_number_of_event).to eq(current_number_of_event)
+            expect(event.errors.messages).to eq({:event_id=>[" : #{event_id} is a NOT valid event_id"]})
+          end
+        end
+
+      end
+
+    end
   end
 end
