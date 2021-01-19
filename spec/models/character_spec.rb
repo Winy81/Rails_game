1e081f7fcc1812e76cd9edaf07d7240872979522
Winy81 <Adam.Nemeth@FootballPools.com>
2021-01-19 14:07:06 +0000

    Add test of age_order_filter method in Character modelt test

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Character, type: :model do
 
+  before :each do
+    @user = User.create(id:1, email: "test_user@email.com", name: "test_user", role: "user")
+    @character_1 = Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 3, user_id: 1 )
+  end
+
   context 'validations' do
 
     context 'built in validations' do
 
-      let(:user) { User.create(id:1, email: "test_user@email.com", name: "test_user", role: "user") }
-      let(:character_1) { Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', user_id: 1 )}
       let(:invalid_char_by_name) { Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', user_id: 2 )}
 
       it { is_expected.to validate_presence_of :name }
@@ -22,14 +25,29 @@ RSpec.describe Character, type: :model do
 
         it 'required unique name' do
 
-          expect(character_1).to be_valid
+          expect(@character_1).to be_valid
           expect(invalid_char_by_name).not_to be_valid
 
         end
     end
-
-
-
-
   end
+
+  describe '#age_order_filter' do
+
+    before do
+      @character_2 = Character.create(name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 1 )
+    end
+
+    it 'has to returned with all character in DESC order by age' do
+
+      ordered_characters = Character.all.age_order_filter
+
+      expect(ordered_characters.count).to eq(2)
+      expect(ordered_characters.first.age).to eq(@character_2.age)
+      expect(ordered_characters.last.age).to eq(@character_1.age)
+
+    end
+  end
+
+
 end
