6ce8b545e353258050b7f07f6204475d3a17afb7
Winy81 <Adam.Nemeth@FootballPools.com>
2021-01-19 10:16:49 +0000

    Test the unique name validation of Character

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Character, type: :model do
 
     context 'built in validations' do
 
+      let(:user) { User.create(id:1, email: "test_user@email.com", name: "test_user", role: "user") }
+      let(:character_1) { Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', user_id: 1 )}
+      let(:invalid_char_by_name) { Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', user_id: 2 )}
+
       it { is_expected.to validate_presence_of :name }
       it { is_expected.to validate_presence_of :fed_state }
       it { is_expected.to validate_presence_of :happiness }
@@ -16,9 +20,16 @@ RSpec.describe Character, type: :model do
 
       it { should validate_length_of(:name).is_at_most(25).with_message('is too long (maximum is 25 characters)') }
 
-      it { should validate_uniqueness_of(:name) }
+        it 'required unique name' do
 
+          expect(character_1).to be_valid
+          expect(invalid_char_by_name).not_to be_valid
+
+        end
     end
+
+
+
 
   end
 end
