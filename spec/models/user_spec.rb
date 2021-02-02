0707c037b43ecde5faa2864165061c01f4f37f60
Winy81 <Adam.Nemeth@FootballPools.com>
2021-02-02 10:57:48 +0000

    Add test for validations for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+include User::Role
+
+RSpec.describe User, type: :model do
+
+  describe 'validations' do
+
+    describe 'built in validations' do
+
+      it { is_expected.to validate_presence_of :name }
+      it { is_expected.to validate_presence_of :role }
+      it do
+        should validate_inclusion_of(:role).in_array(ALL)
+      end
+
+    end
+  end
+
+end
