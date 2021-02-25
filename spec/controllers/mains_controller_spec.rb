1b6452de3f34c884e09208f836a48d873d829ff8
Winy81 <Adam.Nemeth@FootballPools.com>
2021-02-25 07:34:03 +0000

    Test of Characters#index when case of 'if exist characters'

--- spec/controllers/mains_controller_spec.rb
+++ spec/controllers/mains_controller_spec.rb
@@ -20,7 +20,28 @@ describe MainsController, type: :request do
     end
 
     context 'if exist characters' do
-      
+
+      before do
+        Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 3, user_id: 1 )
+        Character.create(name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 1 )
+      end
+
+      let(:all_character) { Character.all }
+      let(:ordered_characters) { all_character.order(:age => :desc) }
+
+      it 'should return with all of the characters in age order' do
+
+        expect(Character).to receive(:all).and_return(all_character)
+
+        expect(all_character).to receive(:age_order_filter).and_return(ordered_characters)
+
+        expect(ordered_characters.first.age).to eq(5)
+        expect(ordered_characters.last.age).to eq(3)
+
+        get mains_path
+
+      end
+
     end
   end
 end
