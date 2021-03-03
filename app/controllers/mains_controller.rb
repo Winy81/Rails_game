465ffe14299e958e190667b3ad866a32586ba3bf
Winy81 <Adam.Nemeth@FootballPools.com>
2021-03-03 09:41:58 +0000

    Test of Characters#leaderboard'

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -6,7 +6,6 @@ class MainsController < ApplicationController
 
   def show
     set_character_details
-    @message = "Hello from mains_controller#index"
     @user = User.find_by(id:@character.user_id)
     @number_of_characters = Character.where(user_id:@user.id)
   end
--- spec/controllers/mains_controller_spec.rb
+++ spec/controllers/mains_controller_spec.rb
@@ -44,4 +44,37 @@ describe MainsController, type: :request do
 
     end
   end
+
+  describe 'GET#show' do
+
+
+  end
+
+  describe 'GET#leaderboard' do
+
+    before do
+      Character.create(name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 3, user_id: 1 )
+      Character.create(name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 9, user_id: 2 )
+      Character.create(name:'character_3',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', age: 30, user_id: 3 )
+      Character.create(name:'character_4',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 90, user_id: 4 )
+      Character.create(name:'character_5',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', age: 75, user_id: 5 )
+    end
+
+    let(:all_character) { Character.all }
+    let(:ordered_characters) { all_character.order(:age => :desc) }
+
+    it 'should return with all of the characters in age order' do
+
+      expect(Character).to receive(:all).and_return(all_character)
+
+      expect(all_character).to receive(:age_order_filter).and_return(ordered_characters)
+
+      expect(ordered_characters.first.age).to eq(90)
+      expect(ordered_characters.last.age).to eq(3)
+
+      get mains_path
+
+    end
+  end
+
 end
