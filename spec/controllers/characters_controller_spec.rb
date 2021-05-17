e81e18424f147dd007b80a0a010595a6982c514f
Winy81 <Adam.Nemeth@FootballPools.com>
2021-05-17 15:45:15 +0100

    Test of Characters_controller#characters_history

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -5,8 +5,9 @@ describe CharactersController, type: :request do
 
   before do
     @current_user = User.create(id:101, email: "test_user@email.com", name: "test_user", role: "user", password:'password')
+    @not_current_user = User.create(id:102, email: "test_user2@email.com", name: "test_user2", role: "user", password:'password')
     Wallet.create(amount: 100, user_id: 101)
-    User.create(id:102, email: "test_user2@email.com", name: "test_user2", role: "user", password:'password')
+    Wallet.create(amount: 100, user_id: 102)
     Character.create(id:1001, name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', died_on: '2021-09-15 18:36:27', age: 3, user_id: 101 )
     Character.create(id:1002, name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 101 )
     Character.create(id:1003, name:'character_3',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', died_on: '2021-09-15 18:36:27',age: 5, user_id: 102 )
@@ -41,13 +42,52 @@ describe CharactersController, type: :request do
       login_as(@current_user)
     end
 
-    it "should return with all of the character belongs for the logged in user in desc order" do
+    it 'should return with all of the character belongs for the logged in user in desc order' do
 
       get all_of_my_character_path
-      
+
       expect(assigns(:characters)).to eq (ordered_characters)
       expect(assigns(:characters).count).to eq(number_of_characters_belongs_current_user)
 
     end
   end
+
+  describe 'GET#characters_history' do
+
+    before do
+      login_as(@current_user)
+    end
+
+    context 'When the user would like to check his own character history' do
+
+      let(:current_user_id) { @current_user.id }
+
+      it 'should be redirected into all_of_my_character page' do
+
+        get characters_history_path(id: current_user_id)
+
+        response.should redirect_to all_of_my_character_path
+
+      end
+
+    end
+
+    context 'When the user would like to check a other users character history' do
+
+      let(:not_current_user_id) { @not_current_user.id }
+      let(:list_of_character_of_not_current_user) { Character.where(user_id:not_current_user_id) }
+
+      it 'should return with all of the users character in desc order but the first is the newest in alive' do
+
+        get characters_history_path(id: not_current_user_id)
+
+        expect(assigns(:owner_of_character)).to eq(@not_current_user)
+        expect(assigns(:owners_characters)).to eq(list_of_character_of_not_current_user.characters_history_order_logic)
+
+      end
+    end
+  end
+
+
+
 end
