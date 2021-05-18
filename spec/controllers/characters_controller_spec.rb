a876cb590584218555c562bbc818a02d23c61a73
Winy81 <Adam.Nemeth@FootballPools.com>
2021-05-18 08:45:41 +0100

    Test of Characters_controller#feeding_deny

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -8,8 +8,8 @@ describe CharactersController, type: :request do
     @not_current_user = User.create(id:102, email: "test_user2@email.com", name: "test_user2", role: "user", password:'password')
     Wallet.create(amount: 100, user_id: 101)
     Wallet.create(amount: 100, user_id: 102)
-    Character.create(id:1001, name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', died_on: '2021-09-15 18:36:27', age: 3, user_id: 101 )
-    Character.create(id:1002, name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 101 )
+    @character_first_user_dead = Character.create(id:1001, name:'character_1',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', died_on: '2021-09-15 18:36:27', age: 3, user_id: 101 )
+    @character_first_user_alive = Character.create(id:1002, name:'character_2',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 101 )
     Character.create(id:1003, name:'character_3',fed_state: 10,happiness:10, activity_require_level: 10, status:'dead', died_on: '2021-09-15 18:36:27',age: 5, user_id: 102 )
     Character.create(id:1004, name:'character_4',fed_state: 10,happiness:10, activity_require_level: 10, status:'alive', age: 5, user_id: 102 )
   end
@@ -85,6 +85,24 @@ describe CharactersController, type: :request do
         expect(assigns(:owners_characters)).to eq(list_of_character_of_not_current_user.characters_history_order_logic)
 
       end
+    end
+  end
+
+  describe 'GET#feeding_deny' do
+
+    before do
+      login_as(@current_user)
+    end
+
+    let(:message) { "Oops, your character has could not finish the meal " }
+
+    it 'should redirect into character_path and flash a message' do
+
+      get feeding_process_path(id: @character_first_user_alive.id)
+
+      expect(flash[:alert]).to eq(message)
+      response.should redirect_to character_path(@character_first_user_alive)
+
     end
   end
 
